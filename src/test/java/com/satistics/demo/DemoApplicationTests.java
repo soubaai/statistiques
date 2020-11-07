e462f2b12f5764adc264b6b3ce3d1d09d3a552a5
soubaai <gsoubaai@gmail.com>
2020-11-07 23:45:33 +0100

    first commit

---
+++ src/test/java/com/satistics/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satistics.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
