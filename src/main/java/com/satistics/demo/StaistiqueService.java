fabdaf276336947925b2aa873f13f415ab209cf3
soubaai <gsoubaai@gmail.com>
2020-11-18 01:26:17 +0100

    update service statistiques

--- src/main/java/com/satistics/demo/StaistiqueService.java
+++ src/main/java/com/satistics/demo/StaistiqueService.java
@@ -2,6 +2,10 @@ package com.satistics.demo;
 
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class StaistiqueService {
+    private String champ1;
 }
